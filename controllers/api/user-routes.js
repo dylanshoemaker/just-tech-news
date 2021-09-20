bbc804cc20c6ce4f4291667991173d213ef153a8
Dylan Shoemaker <me@dylanshoemaker.com>
2021-09-19 17:32:42 -0700

    create login page, add front end logic to forms, create a session on back end (cookies)

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -56,7 +56,15 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbUserData) => res.json(dbUserData))
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -80,8 +88,14 @@ router.post("/login", (req, res) => {
       res.status(400).json({ message: "Incorrect password!" });
       return;
     }
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
-    res.json({ user: dbUserData, message: "You are now logged in!" });
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
   });
 });
 // PUT /api/users/1
